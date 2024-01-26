ea1b7f3a6c288352f158422d81348fa8b4be0ad5
Mateusz Janda <mateusz.janda@gmail.com>
2024-01-26 17:13:18 +0100

    Some blinking logic

--- main.c
+++ main.c
@@ -1,5 +1,27 @@
 #include <at89x52.h>
 
-void main(void) {
+void delay(void);
 
+void main(void)
+{
+    while (1) {
+        // P1.1 high and P1.3 low
+        P1_1 = 1;
+        P1_3 = 0;
+        delay();
+
+        // P1.1 low and P1.3 high
+        P1_1 = 0;
+        P1_3 = 1;
+        delay();
+    }
+}
+
+void delay(void)
+{
+    unsigned int x, y;
+    for (x = 0; x < 200; x++) {
+
+        for (y = 0; y < 200; y++) { }
+    }
 }
