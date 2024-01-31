997fb614620f76fe715f04aecf49d53b1cf1d8ab
Mateusz Janda <mateusz.janda@gmail.com>
2024-01-31 22:36:22 +0100

    Fixed ping configuraion. Lower delay

--- main.c
+++ main.c
@@ -8,19 +8,19 @@ void main(void)
     startup();
 
     while (1) {
+        P1_0 = 1;
+        P1_1 = 0;
+        P1_2 = 0;
+        delay();
+
+        P1_0 = 0;
         P1_1 = 1;
         P1_2 = 0;
-        P1_3 = 0;
         delay();
 
+        P1_0 = 0;
         P1_1 = 0;
         P1_2 = 1;
-        P1_3 = 0;
-        delay();
-
-        P1_1 = 0;
-        P1_2 = 0;
-        P1_3 = 1;
         delay();
     }
 }
@@ -28,21 +28,21 @@ void main(void)
 void startup(void)
 {
     for (int i = 0; i < 3; i++) {
+        P1_0 = 0;
         P1_1 = 0;
         P1_2 = 0;
-        P1_3 = 0;
         delay();
 
+        P1_0 = 1;
         P1_1 = 1;
         P1_2 = 1;
-        P1_3 = 1;
         delay();
     }
 }
 
 void delay(void)
 {
-    for (int x = 0; x < 1000; x++) {
-        for (int y = 0; y < 1275; y++) { }
+    for (int x = 0; x < 100; x++) {
+        for (int y = 0; y < 200; y++) { }
     }
 }
